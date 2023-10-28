3456bb1285aac2426e3727f32986d492617b4197
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-10-28 04:35:17 +0300

    Create sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,6 +13,31 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: 'User created succssfully..' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(401, 'Invalid password'));
+    }
+    const { password: hashedPassword, ...user } = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const expiryDate = new Date(Date.now() + 3600000);
+    res
+      .cookie('auth_app_token', token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(user);
   } catch (error) {
     next(error);
   }
