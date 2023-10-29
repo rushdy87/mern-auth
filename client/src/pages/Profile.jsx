22b2dfc9608b6dff7befd9d3a69c123619f77588
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-10-29 04:59:09 +0300

    Complete profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,47 @@
+import { useSelector } from 'react-redux';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img
+          src={currentUser.profilePicture}
+          alt={currentUser.username}
+          className='h-24 w-24 rounded-full object-cover self-center cursor-pointer mt-2'
+        />
+        <input
+          type='text'
+          id='username'
+          placeholder='Username'
+          defaultValue={currentUser.username}
+          className='bg-slate-100 rounded-lg p-3'
+        />
+        <input
+          type='email'
+          id='email'
+          placeholder='Email'
+          defaultValue={currentUser.email}
+          className='bg-slate-100 rounded-lg p-3'
+        />
+        <input
+          type='password'
+          id='password'
+          placeholder='Password'
+          className='bg-slate-100 rounded-lg p-3'
+        />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-75'>
+          Update
+        </button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
